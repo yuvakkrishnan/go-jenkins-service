026dff3e5148881e776c57317799891998be623b
Yuvakkrishnan <yuvakkrishnan@gmail.com>
2025-06-26 22:56:24 +0530

    third brach to check the jenkins

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello from Go!")
 }
 
+func subsituteHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Go!-subsituteHandler")
+}
+
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/sub", subsituteHandler)
+
 	log.Println("Starting server on :8081...")
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
